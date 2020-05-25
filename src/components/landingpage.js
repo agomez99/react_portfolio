958904838af174322c1f5ce8a6e15e43718570ed
agomez99 <austine.gomez@yahoo.com>
2020-05-25 18:29:29 -0500

    syled, added content to langing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,12 +1,61 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
+
 
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing Page</h1>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://ag2portfolio.herokuapp.com/images/avataaars.png"
+              alt="avatar"
+              className="avatar-img"
+            />
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+              <hr />
+
+              <p>HTML/CSS | Javascript| NodeJS</p>
+              <div className="social-links">
+                <a
+                  href="https://www.linkedin.com/in/austine-gomez/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+
+                <a
+                  href="https://github.com/agomez99"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+
+                <a
+                  href="https://www.freecodecamp.org/agomez99"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-free-code-camp" aria-hidden="true" />
+                </a>
+                <a
+                  href="https://twitter.com/austine_gomez"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-twitter-square" aria-hidden="true" />
+                </a>
+
+              </div>
+            </div>
+          </Cell>
+        </Grid>
       </div>
-    )
+    );
   }
 }
 
