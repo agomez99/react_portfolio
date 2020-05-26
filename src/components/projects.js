7e6bd874a4790fe2c6f952c912594a7e7f76dabf
agomez99 <austine.gomez@yahoo.com>
2020-05-25 19:12:33 -0500

    added projects section

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,12 +1,189 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton,
+  CardText,
+} from "react-mdl";
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div className="projects-grid">
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://dwglogo.com/wp-content/uploads/2017/09/1460px-React_logo.png)center/ cover",
+              }}
+            >
+              React Project
+            </CardTitle>
+            <CardText>xxxxxxxxxxxxxx</CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>Github</Button>
+              <Button colored>Github</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://dwglogo.com/wp-content/uploads/2017/09/1460px-React_logo.png)center/ cover",
+              }}
+            >
+              React Project
+            </CardTitle>
+            <CardText>xxxxxxxxxxxxxx</CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>Github</Button>
+              <Button colored>Github</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#fff",
+                height: "176px",
+                background:
+                  "url(https://dwglogo.com/wp-content/uploads/2017/09/1460px-React_logo.png)center/ cover",
+              }}
+            >
+              React Project
+            </CardTitle>
+            <CardText>xxxxxxxxxxxxxx</CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>Github</Button>
+              <Button colored>Github</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background:
+                "url(https://dwglogo.com/wp-content/uploads/2017/09/1460px-React_logo.png)center/ cover",
+            }}
+          >
+            React Project
+          </CardTitle>
+          <CardText>xxxxxxxxxxxxxx</CardText>
+          <CardActions border>
+            <Button colored>Github</Button>
+            <Button colored>Github</Button>
+            <Button colored>Github</Button>
+          </CardActions>
+          <CardMenu style={{ color: "#fff" }}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background:
+                "url(https://dwglogo.com/wp-content/uploads/2017/09/1460px-React_logo.png)center/ cover",
+            }}
+          >
+            React Project
+          </CardTitle>
+          <CardText>xxxxxxxxxxxxxx</CardText>
+          <CardActions border>
+            <Button colored>Github</Button>
+            <Button colored>Github</Button>
+            <Button colored>Github</Button>
+          </CardActions>
+          <CardMenu style={{ color: "#fff" }}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
+      );
+    } else if (this.state.activeTab === 3) {
+      return (
+        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background:
+                "url(https://dwglogo.com/wp-content/uploads/2017/09/1460px-React_logo.png)center/ cover",
+            }}
+          >
+            React Project
+          </CardTitle>
+          <CardText>xxxxxxxxxxxxxx</CardText>
+          <CardActions border>
+            <Button colored>Github</Button>
+            <Button colored>Github</Button>
+            <Button colored>Github</Button>
+          </CardActions>
+          <CardMenu style={{ color: "#fff" }}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
+      );
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>Projects Page</h1>
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={(tabId) => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>Angular</Tab>
+          <Tab>VueJS</Tab>
+          <Tab>MongoDB</Tab>
+        </Tabs>
+
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
       </div>
-    )
+    );
   }
 }
 
