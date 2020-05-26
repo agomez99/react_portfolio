1259e2d01252b6c6d2bd095a3170f614ada82866
agomez99 <austine.gomez@yahoo.com>
2020-05-26 17:50:40 -0500

    add contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,48 @@
 import React, {Component} from 'react'
+import {Grid, Cell, List, ListItem, ListItemContent} from 'react-mdl';
 
 class Contact extends Component {
   render() {
     return (
-      <div>
-        <h1>Contacts Page</h1>
-      </div>
+   <div className =" contact-body">
+     <Grid className= " contact-grid">
+       <Cell col={6}> 
+       <h2>
+         Austine Gomez
+       </h2>
+       <img 
+       src="https://ag2portfolio.herokuapp.com/images/avataaars.png"
+       alt="avatar"
+       style={{height: '250px'}}
+       />
+       <p style={{width:'75%', margin: 'auto', paddingTop: '1em' }}>filllllllllllllllllllllllllllllllll</p>
+       </Cell>
+       <Cell col={6}>
+         <h2>Contact Me</h2>
+         <hr />
+
+         <div className= "contact-list">
+         <List>
+            <ListItem>
+              <ListItemContent style={{fontSize:'30px', fontFamily:'Anton'}}>
+                <i className="fa fa-phone-square" aria-hidden="true" />
+                210-589 8981</ListItemContent>
+            </ListItem>
+            <ListItem>
+              <ListItemContent style={{fontSize:'30px', fontFamily:'Anton'}}>
+                <i className="fa fa-envelope" aria-hidden="true" />
+                agdevelop3r@gmail.com</ListItemContent>
+            </ListItem>
+            <ListItem>
+              <ListItemContent style={{fontSize:'30px', fontFamily:'Anton'}}>
+                <i className="fa fa-linkedin" aria-hidden="true" />
+                linkedin.com/in/austine-gomez</ListItemContent>
+            </ListItem>
+          </List>
+          </div>
+       </Cell>
+     </Grid>
+   </div>
     )
   }
 }
