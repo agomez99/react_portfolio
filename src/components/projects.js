7440b33527376633aa21c93e290842a67a0baf15
Austine Gomez <austine.gomez@yahoo.com>
2020-06-07 12:54:52 -0500

    css

--- src/components/projects.js
+++ src/components/projects.js
@@ -15,54 +15,47 @@ class Projects extends Component {
   toggleCategories() {
     if (this.state.activeTab === 0) {
       return (
-        <div >
-          <Container >
+        <div  >
+          <Container className="projects">
             <Row>
-              <Col style={{ display: 'flex', margin: "auto", flexDirection: 'row', }}>
-
+              <Col  style={{ display: 'flex', margin: "auto", flexDirection: 'row', }}>
                 <Card style={{ width: '30rem' }}>
                   <Card.Title
                     style={{
-                      color: "black",
                       height: "176px",
                       background:
                         "url(https://ag2portfolio.herokuapp.com/images/note.jpg)center/ cover",
                     }}>
-                    Note Taker
             </Card.Title>
+                  <h4>Note Taker</h4>
                   <Card.Text>Application for taking notes with ExpressJS</Card.Text>
                   <Card.Text><Button colored><a href="url">Github</a></Button></Card.Text>
                 </Card>
 
-
-
                 <Card style={{ width: '30rem' }}>
                   <Card.Title
                     style={{
-                      color: "black",
                       height: "176px",
                       background:
                         "url(http://ag2portfolio.herokuapp.com/images/burger.jpg)center/ cover",
                     }}>
-                    Eat-Da-Burger
             </Card.Title>
+            <h4>Eat Da Burger</h4>
                   <Card.Text>Application  using MySQL, Node, Express, Handlebars, homemade ORM, NES.CSS
-              Deployed with JawsDB on Heroku</Card.Text>
+                    Deployed with JawsDB on Heroku</Card.Text>
                   <Card.Text><Button colored><a href="url">Github</a></Button></Card.Text>
                 </Card>
 
                 <Card style={{ width: '30rem' }}>
                   <Card.Title
                     style={{
-                      color: "black",
                       height: "176px",
                       background:
                         "url(https://ag2portfolio.herokuapp.com/images/workout.jpg)center/ cover",
                     }}>
-                    Workout Tracker
             </Card.Title>
+            <h4>Workout Tracker</h4>
                   <Card.Text>Application for tracking workouts using a Mongo database with a Mongoose schema and handles routes with Express</Card.Text>
-
                   <Card.Text><Button colored><a href="url">Github</a></Button></Card.Text>
                 </Card>
               </Col>
@@ -71,32 +64,25 @@ class Projects extends Component {
 
             <Row>
               <Col style={{ display: 'flex', flexDirection: 'row', }}>
-
                 <Card style={{ width: '30rem' }}>
                   <Card.Title
                     style={{
-                      color: "black",
                       height: "176px",
                       background:
                         "url(https://ag2portfolio.herokuapp.com/images/note.jpg)center/ cover",
                     }}>
-                    Note Taker
             </Card.Title>
                   <Card.Text>Application for taking notes with ExpressJS</Card.Text>
                   <Card.Text><Button colored><a href="url">Github</a></Button></Card.Text>
                 </Card>
 
-
-
                 <Card style={{ width: '30rem' }}>
                   <Card.Title
                     style={{
-                      color: "black",
                       height: "176px",
                       background:
                         "url(http://ag2portfolio.herokuapp.com/images/burger.jpg)center/ cover",
                     }}>
-                    Eat-Da-Burger
             </Card.Title>
                   <Card.Text>Application  using MySQL, Node, Express, Handlebars, homemade ORM, NES.CSS
               Deployed with JawsDB on Heroku</Card.Text>
@@ -106,15 +92,12 @@ class Projects extends Component {
                 <Card style={{ width: '30rem' }}>
                   <Card.Title
                     style={{
-                      color: "black",
                       height: "176px",
                       background:
                         "url(https://ag2portfolio.herokuapp.com/images/workout.jpg)center/ cover",
                     }}>
-                    Workout Tracker
             </Card.Title>
                   <Card.Text>Application for tracking workouts using a Mongo database with a Mongoose schema and handles routes with Express</Card.Text>
-
                   <Card.Text><Button colored><a href="url">Github</a></Button></Card.Text>
                 </Card>
               </Col>
@@ -124,9 +107,7 @@ class Projects extends Component {
       );
     } else if (this.state.activeTab === 1) {
       return (
-        <div className="projects-grid">
-
-
+        <div className="projects">
           <Container >
             <Row>
               <Col className="card" style={{ display: 'flex', margin: "auto", flexDirection: 'row', }}>
@@ -134,13 +115,12 @@ class Projects extends Component {
                 <Card style={{ width: '30rem' }}>
                   <Card.Title
                     style={{
-                      color: "black",
-                      height: "176px",
+                      height: "190px",
                       background:
                         "url(https://ag2portfolio.herokuapp.com/images/station.jpg)center/ cover",
                     }}>
-                    Station Trak
             </Card.Title>
+                  <h4>Station Trak</h4>
                   <Card.Text>Application for trakcking the ISS using the API</Card.Text>
                   <Card.Text><Button colored><a href="url">Github</a></Button></Card.Text>
                 </Card>
@@ -148,20 +128,18 @@ class Projects extends Component {
                 <Card style={{ width: '30rem' }}>
                   <Card.Title
                     style={{
-                      color: "black",
                       height: "176px",
                       background:
                         "url(https://ag2portfolio.herokuapp.com/images/karikari.jpg?crop=entropy&fit=crop&fm=jpg)center/ cover",
                     }}>
-                    Eat-Da-Burger
             </Card.Title>
+                    <h4>KariKari</h4>
                   <Card.Text>Pictionary type drawing online game using SocketIO</Card.Text>
                   <Card.Text><Button colored><a href="url">Github</a></Button></Card.Text>
                 </Card>
               </Col>
             </Row>
           </Container>
-
         </div>
       );
 
@@ -179,9 +157,8 @@ class Projects extends Component {
           <Tab>Recent Work</Tab>
           <Tab>Group Projects</Tab>
         </Tabs>
-
         <Container>
-          <Col sm={12}>
+          <Col >
             <div className="content">{this.toggleCategories()}</div>
           </Col>
         </Container>
