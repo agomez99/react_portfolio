d4e5bc29982c55b1d7e32a14579d078b8772e71a
Austine Gomez <austine.gomez@yahoo.com>
2020-06-08 23:08:33 -0500

    header and footer component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Layout, Drawer, Content } from "react-mdl";
 import Main from "./components/main";
-import { Nav, Navbar} from "react-bootstrap";
-import { Link } from "react-router-dom";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div >
-
-<Navbar className="header-color" >
-    <Navbar.Brand href="#home" >Austine Gomez</Navbar.Brand>
-    <Nav className="justify-content-end" style={{ width:"100%"}}>
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/aboutme">About Me</Nav.Link>
-      <Nav.Link href="/projects">Projects</Nav.Link>
-      <Nav.Link href="/resume">Resume</Nav.Link>
-      <Nav.Link href="/contact">Contact</Nav.Link>
-
-    </Nav>
- 
-  </Navbar>
-
-
-        <Layout  >
-
-          <Drawer title="Title">
-            <Nav>
-              <Link to="/">Home</Link>
-              <Link to="/resume">Resume</Link>
-              <Link to="/aboutme">AboutMe</Link>
-              <Link to="/projects">Projects</Link>
-              <Link to="/contact">Contacts</Link>
-            </Nav>
-          </Drawer>
-          <Content>
-            <div className="page-content" />
-            <Main />
-          </Content>
-        </Layout>
+        <Header />
+        <Main />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+import { Nav, Navbar} from "react-bootstrap";
+
+
+
+class Header extends Component {
+    render() {
+        return (
+            <div >
+                <Navbar className="header-color" >
+                    <Navbar.Brand href="#home" >Austine Gomez</Navbar.Brand>
+                    <Nav className="justify-content-end" style={{ width: "100%" }}>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/aboutme">About Me</Nav.Link>
+                        <Nav.Link href="/projects">Projects</Nav.Link>
+                        <Nav.Link href="/resume">Resume</Nav.Link>
+                        <Nav.Link href="/contact">Contact</Nav.Link>
+
+                    </Nav>
+
+                </Navbar>
+            </div>
+
+        );
+    }
+}
+
+export default Header;
+
+
+
