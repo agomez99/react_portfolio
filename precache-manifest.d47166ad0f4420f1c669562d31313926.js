c6a72fd83c37f4048a2b9dc80eb72cbb0fed8314
Austine Gomez <austine.gomez@yahoo.com>
2020-06-09 20:58:05 -0500

    Updates

---
+++ precache-manifest.d47166ad0f4420f1c669562d31313926.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93cb299c75b317f5617ee56061fbd564",
+    "url": "/agomez99/react_portfolio/index.html"
+  },
+  {
+    "revision": "8c59b99a803daee722dc",
+    "url": "/agomez99/react_portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "4c3a75c25bdc9383dc3f",
+    "url": "/agomez99/react_portfolio/static/css/main.ffe8f681.chunk.css"
+  },
+  {
+    "revision": "8c59b99a803daee722dc",
+    "url": "/agomez99/react_portfolio/static/js/2.8d30cdea.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/agomez99/react_portfolio/static/js/2.8d30cdea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3a75c25bdc9383dc3f",
+    "url": "/agomez99/react_portfolio/static/js/main.485f63e5.chunk.js"
+  },
+  {
+    "revision": "168c79ef5b00a3bd474d",
+    "url": "/agomez99/react_portfolio/static/js/runtime-main.f0ca3888.js"
+  }
+]);
